d9fcdee84cce422d26b5ad55208da74b4d5683aa
Brian Harney <bharney0@gmail.com>
2019-02-13 21:19:26 -0800

    Starting point for setup cosmosdb nosql with keyvualt in node.

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const config = {
+  dbUrl: 'mongodb://localhost:27017/api-design',
+  // clientId: "508b0e51-c020-49b8-9846-072a87d8838d",
+  // clientSecret: "UJdxlkkgnOm736BqdgA5wbgfgE1Anzs+dFh0zQ7zRdE=",
+  // vaultUri: "https://node-cosmosdb-starter.azurewebsites.net/",
+  // tenantId: "32e1d8d3-bc39-4b94-9c4d-4192d41b2a63",
+  // subscriptionId: "f9344551-eed5-47b3-a8f4-46fa282af927"
+}
---
+++ src/server.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from 'apollo-server'
+import { loadTypeSchema } from './utils/schema'
+import { authenticate } from './utils/auth'
+import { merge } from 'lodash'
+import mongoose from 'mongoose'
+import product from './types/product/product.resolvers'
+import coupon from './types/coupon/coupon.resolvers'
+import user from './types/user/user.resolvers'
+
+const types = ['product', 'coupon', 'user']
+
+export const start = async (connectionString) => {
+  console.log(connectionString);
+  const rootSchema = `
+    schema {
+      query: Query
+      mutation: Mutation
+    }
+  `
+  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
+
+  const server = new ApolloServer({
+    typeDefs: [rootSchema, ...schemaTypes],
+    resolvers: merge({}, product, coupon, user),
+    async context({ req }) {
+      const user = await authenticate(req)
+      return { user }
+    }
+  })
+
+  await mongoose.connect(connectionString)
+  const { url } = await server.listen({ port: config.port })
+
+  console.log(`GQL server ready at ${url}`)
+}
